da7911e0dd383b3facec771858cc7a3e51fa21e7
Your <you@example.com>
2025-02-26 08:43:56 -0800

    remove unused code

--- components/WalletButton.tsx
+++ components/WalletButton.tsx
@@ -6,7 +6,6 @@ import React from 'react'
 export default function SimpleWalletButton() {
   return (
     <>
-      {/* @ts-expect-error Suppressing TypeScript errors for React 19 global components */}
       <appkit-button />
     </>
   )
